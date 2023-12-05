9f24da7a274795444dd95f457420cbc469b04cc2
Milan Vlaskovic <milan.vlaskovic@devtechgroup.com>
2023-12-05 16:17:05 +0100

    first for loop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>{{ title }}</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h2>{{ subTitle }}</h2>
+    <ul id="task-list">
+       <li v-for="link in links">
+         <a :href="link.url">{{ link.title }}</a>
+       </li>
+     </ul>
   </div>
 </template>
 
@@ -88,8 +15,26 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      title: 'Some cool links'
+      msg: 'Passing data to template',
+      subTitle: 'Use loop itteration',
+      links: [
+        {
+          'title': 'Core Docs',
+          'url': 'https://vuejs.org'
+        },
+        {
+          'title': 'Forum',
+          'url': 'https://forum.vuejs.org'
+        },
+        {
+          'title': 'Community Chat',
+          'url': 'https://chat.vuejs.org'
+        },
+        {
+          'title': 'Templates',
+          'url': 'http://vuejs-templates.github.io/webpack/'
+        },
+      ]
     }
   }
 }
